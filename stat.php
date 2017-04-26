c92e2c6104f135be26bc6086e38263da32e42ef1
Andrey Shkutin <andreyshkutin@rambler.ru>
2017-04-26 18:59:05 +0800

    2 поля ввода

--- stat.php
+++ stat.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Крестики-нолики.</title>
+  <link rel="stylesheet" type="text/css" href="" />
+</head>
+<body>
+<p align="center">
 <?php
   $user1 = $_POST['pname1'];
   $user2 = $_POST['pname2'];
@@ -8,3 +18,24 @@
   
   
  ?>
+ </p>
+ 
+ <table bordercolor="red" border=2 align="center" >
+ <tr><td height="30" width="30" align= "center">?</td><td height="30" width="30" align= "center">?</td><td height="30" width="30" align= "center">?</td></tr>
+ <tr><td height="30" width="30" align= "center">?</td><td height="30" width="30" align= "center">?</td><td height="30" width="30" align= "center">?</td></tr>
+ <tr><td height="30" width="30" align= "center">?</td><td height="30" width="30" align= "center">?</td><td height="30" width="30" align= "center">?</td></tr>
+ </table>
+ 
+ <table bordercolor="blue" border=2 align="center" >
+ <tr><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td></tr>
+ <tr><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td></tr>
+ <tr><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="0" /></td></tr>
+ </table>
+ 
+  <table bordercolor="green" border=2 align="center" >
+ <tr><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td></tr>
+ <tr><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td></tr>
+ <tr><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td><td height="30" width="30" align= "center"><input type="submit" name="submit" value="X" /></td></tr>
+ </table>
+ </body>
+</html>
