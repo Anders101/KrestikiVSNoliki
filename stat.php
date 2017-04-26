283f6e5a8684045273d8e2c969c6ef30f0b6907e
Andrey Shkutin <andreyshkutin@rambler.ru>
2017-04-26 17:09:09 +0800

    Добавлен сценарий и счет

---
+++ stat.php
@@ -0,0 +1,10 @@
+<?php
+  $user1 = $_POST['pname1'];
+  $user2 = $_POST['pname2'];
+  $score1 = 0;
+  $score2 = 0;
+  
+  echo ' '. $user1 . ' ' . $score1 . ':' . $score2 . ' ' . $user2;
+  
+  
+ ?>
